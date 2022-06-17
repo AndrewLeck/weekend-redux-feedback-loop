b98b951f8fb06bc4c2db410903c5290026fe743a
Andrew Leck <Aleck360@gmail.com>
2022-06-17 02:13:57 -0500

    created a routes folder and a feedBackRoutes.js for the server side GET

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+const feedBackRouter = require('./routes/feedBack.Router.js')
+app.use('/api/')
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+import logger from 'redux-logger'
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-
+const feedBackList = (state = [], action) => {
+    switch(action.type){
+        case 'SET_FEEDBACK' :
+            return action.payload
+    }
+    return state
+}
 
 const feedBackStore = createStore(
     combineReducers({
-
+        feedBackList
     }),
     applyMiddleware(logger)
 );
